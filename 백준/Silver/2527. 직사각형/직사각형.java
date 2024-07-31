ea845b110b86d1293c8b46d494ab2418e7c50c8b
Ethan-lsh <76682896+Ethan-lsh@users.noreply.github.com>
2024-08-01 00:21:11 +0900

    [Silver I] Title: 직사각형, Time: 76 ms, Memory: 11740 KB -BaekjoonHub

--- 백준/Silver/2527. 직사각형/직사각형.java
+++ 백준/Silver/2527. 직사각형/직사각형.java
@@ -1,8 +1,12 @@
 import java.util.*;
 import java.io.*;
 
+/**
+ * 두 개의 직사각형의 좌하단, 우상단의 좌표가 주어졌을 때 서로 곂치는 영역의 모양을 확인하는 문제
+ * 수학적 이해를 요구하는 문제이다.
+ * 각 경우의 수를 성립하게 하는 조건을 모두 고려해야 한다. 편법은 없고 조건을 잘 생각해야 하는 꽤 까다로운 문제
+ */
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
@@ -28,17 +32,15 @@ public class Main {
 			} else if ((p1 == x2 && q1 == y2) || (p1==x2 && y1 == q2) || 
 					(x1 == p2 && q1 == y2) || (x1 == p2 && y1 == q2)) {
 				System.out.println('c');
-			} 
-			else if ((p1 == x2 || x1 == p2) && (y1 <= q2 && q1 >= y2)) {
+			} else if ((p1 == x2 || x1 == p2) && (y1 <= q2 && q1 >= y2)) {
 				System.out.println('b');
 			} else if ((q1 == y2 || y1 == q2) && (x1 <= p2 && p1 >= x2)) {
 				System.out.println('b');
-			}			
-			else {
+			} else {
 				System.out.println('a');
-			}
-		}
-	
+			} // end if
+			
+		} // end for
 	}
 
 }
