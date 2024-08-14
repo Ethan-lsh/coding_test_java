2208de14a626d600a95173ae5103d8a2ba3230fe
Ethan-lsh <76682896+Ethan-lsh@users.noreply.github.com>
2024-08-14 16:31:31 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 64 ms, Memory: 11520 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static int sum; // 아홉 난장이 숫자의 합
+	static int[] arr; // 아홉 난장이의 값을 담는 배열
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arr = new int[9];
+        int sum = 0;
+        for(int i = 0; i < 9; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+            sum += arr[i];
+        }
+
+        for(int i = 0; i < 8; i++) {
+            for(int j = 1; j < 9; j++) {
+                if(sum - arr[i] - arr[j] == 100) {
+                    for(int k=0; k < 9; k++) {
+                        if(arr[k] != arr[i] && arr[k] != arr[j]) {
+                            System.out.println(arr[k]);
+                        }
+                    }
+                    return ;
+                }
+            }
+        }
+
+	}
+}
