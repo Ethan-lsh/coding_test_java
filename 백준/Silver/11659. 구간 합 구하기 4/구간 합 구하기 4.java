bd7caf09c6fefd9662b42f7beb83d11bb5af8969
Ethan-lsh <76682896+Ethan-lsh@users.noreply.github.com>
2024-08-14 16:47:18 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 784 ms, Memory: 89260 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,49 +1,40 @@
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Main {
 
-	static int[] arr;
-	static int[] sum;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		arr = new int[N+1];
-		sum = new int[N+1];
-		
-		for (int i = 1; i <= N; i++) {
-			int num = Integer.parseInt(st.nextToken());
-			arr[i] = num;
-			sum[i] += sum[i-1] + arr[i];
-		}
-		
-//		System.out.println(Arrays.toString(sum));
-		
-		for (int m = 0; m < M; m++) {
-			
-			st = new StringTokenizer(br.readLine());
-			
-			int i = Integer.parseInt(st.nextToken());
-			int j = Integer.parseInt(st.nextToken());
-			
-			int result = sum[j] - sum[i-1];
-			
-			sb.append(result + "\n");
-		}
-		
-		System.out.println(sb.toString());
-		
-	}
+    static int N;
+    static int M;
+    static int allSum;
+    static int[] sumArr;
+    static StringBuilder sb = new StringBuilder();
 
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] input1 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        N = input1[0];
+        M = input1[1];
+        sumArr = new int[N + 1];
+
+        int[] inputArr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for(int i = 0; i < N; i++) {
+            allSum += inputArr[i];
+            sumArr[i + 1] = allSum;
+        }
+
+        for(int i = 0; i < M; i++) {
+            int[] input2 = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int start = input2[0];
+            int end = input2[1];
+
+            sb.append(sumArr[end] - sumArr[start - 1] + "\n");
+        }
+        
+        System.out.print(sb);
+
+    }
+    
 }
