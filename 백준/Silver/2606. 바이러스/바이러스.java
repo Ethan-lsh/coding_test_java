705b115df7058023bfb22c1effdf58a9830c4ef0
Ethan-lsh <76682896+Ethan-lsh@users.noreply.github.com>
2024-09-02 23:44:36 +0900

    [Silver III] Title: 바이러스, Time: 108 ms, Memory: 13276 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,10 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * 메모리:13364kb, 실행시간:108ms
+ */
 public class Main {
 	
-	static List<List<Integer>> graph;
-	static boolean[] visited;
+	static List<List<Integer>> graph;  // 그래프
+	static boolean[] visited;          // 방문 노드
 	static int ans;
 	
 	public static void main(String[] args) {
@@ -14,6 +17,7 @@ public class Main {
 		
 		int N = sc.nextInt();
 		
+		// ArrayList 중첩 구문 생성
 		graph = new ArrayList<>();
 		for (int i = 0; i <= N; i++) {
 			graph.add(new ArrayList<>());
@@ -22,6 +26,8 @@ public class Main {
 		int E = sc.nextInt();
 		
 		
+		// 무방향 그래프 생성
+		// (1, 2) (2, 1)은 같은 간선
 		for (int e = 0; e < E; e++) {
 			int a = sc.nextInt();
 			int b = sc.nextInt();
@@ -31,18 +37,19 @@ public class Main {
 		}
 		
 		visited = new boolean[N+1];
-		bfs(1);
+		dfs(1);
 		
 		System.out.println(ans-1);
 	}
 	
-	static void bfs(int node) {
+	static void dfs(int node) {
 		
 		visited[node] = true;
 		ans++;
 		
+		// 현재 노드에 연결된 모든 노드에 대해 탐색
 		for (int next : graph.get(node)) {
-			if (!visited[next]) bfs(next);
+			if (!visited[next]) dfs(next);
 		}
 		
 		return;
