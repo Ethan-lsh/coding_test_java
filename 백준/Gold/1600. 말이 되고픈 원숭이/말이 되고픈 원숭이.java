c4dc34ec7d48ea70db5b79592193a02e3b0c1e21
Ethan-lsh <76682896+Ethan-lsh@users.noreply.github.com>
2024-09-06 14:42:08 +0900

    Update 말이 되고픈 원숭이.java

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -1,3 +1,5 @@
+package algo0906;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,11 +7,14 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Main {
+/**
+ * 메모리:49724kb, 실행시간:388ms
+ */
+public class Main_B_1600_말이되고픈원숭이 {
 
 	static int K, W, H, board[][];
 	static int[][][] visited; // (r, c) 말의 이동 여부체크
-
+ 
 	static int ans;
 
 	static int[][] drc = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } }; // 4방 탐색
@@ -53,7 +58,16 @@ public class Main {
 	private static void bfs() {
 		Queue<Node> q = new ArrayDeque<>();
 
-		boolean[][][] visited = new boolean[H][W][K+1];
+		/**
+		 * 방문 좌표는 3차원 배열로 설정한다
+		 * 1열과 2열은 각각 행과 열의 위치를 나타내고, 3열은 K값일 때 방문 여부를 나타낸다
+		 * 이는, 우리가 좌표를 방문할 수 있는 방법이 걷는것과 뛰는 것 두개로 구분되기 때문이다.
+		 * 예를 들어, (1, 2) 좌표를 임의의 출발점에서 걸어서 갈 수도 있고, 뛰어서 도착할 수도 있다.
+		 * 그런데 만약 방문 배열을 2차원 배열을 쓰게되면, 걸어서 간 경우에 대해 방문처리를 했으므로 뛰어갈 수 있는 경우를 막아버린다
+		 * 즉, 서로 독립적인 케이스로 고려해야 하기 때문에 방문 배열을 K개 만큼 설정해주어야 한다.
+		 * 또한, k==2 에서 k==3 일때 (0, 0) -> (1, 2); k==2에서 k==3 일 때 (1, 1) -> (1, 2) 인 경우는 같은 k값에 이미 방문한 좌표이기 때문에 패스하는 것도 고려해야 한다.
+		 */
+		boolean[][][] visited = new boolean[H][W][K+1]                                                                                                                                                                                                                        ;
 		
 		q.offer(new Node(0, 0, 0));  // cnt에는 말처럼 이동할 수 있는 횟수인 K를 준다
 		
@@ -79,9 +93,9 @@ public class Main {
 					int nr = r + drc[d][0];
 					int nc = c + drc[d][1];
 					
-					if (!isInRange(nr, nc)) continue;
-					if (board[nr][nc] == 1) continue;
-					if (visited[nr][nc][k]) continue;
+					if (!isInRange(nr, nc)) continue;  // 범위를 벗어남
+					if (board[nr][nc] == 1) continue;  // 도착지점에 장애물이 있는 경우
+					if (visited[nr][nc][k]) continue;  // k 시점에 방문한 적이 있는 경우
 					
 					q.offer(new Node(nr, nc, k));
 					visited[nr][nc][k] = true;
@@ -90,13 +104,14 @@ public class Main {
 				if (k == K) continue;
 				
 				
+				// 말이 뛰기 때문에, K+1
 				for (int d = 0; d < 8; d++) {
 					int nr = r + hrc[d][0];
 					int nc = c + hrc[d][1];
 					
 					if (!isInRange(nr, nc)) continue;  // 범위를 벗어남						
 					if (board[nr][nc] == 1) continue;  // 도착지점에 장애물이 있는 경우
-					if (visited[nr][nc][k+1]) continue;
+					if (visited[nr][nc][k+1]) continue;  // k+1 시점에 방문한 적이 있는 경우
 					
 					q.offer(new Node(nr, nc, k+1));
 					visited[nr][nc][k+1] = true;
