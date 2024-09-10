74167a4597e53ab23c9eec9838af7bd88f318c07
Ethan-lsh <76682896+Ethan-lsh@users.noreply.github.com>
2024-09-11 01:00:36 +0900

    Update 피보나치 함수.java

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class Main_B {
 
 	static int[][] arr;
 	
@@ -19,10 +19,11 @@ public class Main {
 				continue;
 			}
 			
+			// 숫자 N이 가질 수 있는 0과 1의 개수를 저장할 배열
 			arr = new int[N+1][2];
 			
-			arr[0][0] = 1;
-			arr[1][1] = 1;
+			arr[0][0] = 1;  // 0 -> 0:1개, 1:0개
+			arr[1][1] = 1;  // 1 -> 0:0개, 1:1개
 			
 			for (int i = 2; i <= N; i++) {
 				arr[i][0] = arr[i-2][0] + arr[i-1][0];
