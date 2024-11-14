d8a9af2391fde3a5cbf5811839c00d50737680db
Ethan-lsh <76682896+Ethan-lsh@users.noreply.github.com>
2024-11-15 01:21:41 +0900

    [Gold III] Title: 파티, Time: 136 ms, Memory: 17004 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -6,16 +6,20 @@ import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
+/**
+ * Dijkstra 문제
+ *
+ */
 public class Main {
 	
-	static int N, M, X;
-	static ArrayList<ArrayList<Edge>> adjList;
+	static int N, M, X;  // 노드의 개수, 간선의 개수, 도착점   
 	
-	static int[] goDist;
-	static int[] backDist;
+	// 가는 방향의 간선, 오는 방향의 간선
+	static ArrayList<ArrayList<Edge>> goEdges, reverseEdges;
+	
+	// 도착지로 가는 최소 가중치 배열, 돌아오는 최소 가중치 배열
+	static int[] goDist, reverseDist;
 
-	static int[] ans;
-	
 	static final int INF = Integer.MAX_VALUE;
 	
 	static class Edge implements Comparable<Edge>{
@@ -42,15 +46,13 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		X = Integer.parseInt(st.nextToken());
-		
-		adjList = new ArrayList<>();
-		for (int i = 0; i <= N; i++) adjList.add(new ArrayList<>());
-		
-		goDist = new int[N+1];
-		Arrays.fill(goDist, INF);
-		
-		backDist = new int[N+1];
-		Arrays.fill(backDist, INF);
+
+		goEdges = new ArrayList<>();
+		reverseEdges = new ArrayList<>();
+		for (int i = 0; i <= N; i++) {
+			goEdges.add(new ArrayList<>());
+			reverseEdges.add(new ArrayList<>());
+		}
 		
 		for (int i = 0; i < M; i++) {
 			
@@ -60,74 +62,62 @@ public class Main {
 			int to = Integer.parseInt(st.nextToken());
 			int weight = Integer.parseInt(st.nextToken());
 			
-			adjList.get(from).add(new Edge(to, weight));
+			/*
+			 * 이 문제의 핵심은 출발지와 도착지를 통일해서 다익스트라 알고리즘을 수행하는 것이 최적화의 가장 큰 부분이다.
+			 * 기존에 from -> to 까지의 간선 정보는 i번째 친구가 X로 가는 간선의 가중치를 저장한다.
+			 * 반면에, to -> from 까지의 간선 정보는 X에서 i번째 친구가 있는 곳으로 가중치를 저장하는 것과 같다
+			 * 예를 들어, '1 -> X = 2' 'X -> 1 = 4' 라고 가정한다면
+			 * X의 관점에서 1로 갈 때 2와 4의 가중치를 더하면 왕복 운동과 동일한 효과를 내는 것이다.
+			 */
+			goEdges.get(from).add(new Edge(to, weight));
+			reverseEdges.get(to).add(new Edge(from, weight));
 		}
 		
-		ans = new int[N+1];
+		// 최소 가중치 배열 초기화
+		goDist = new int[N+1];
+		reverseDist = new int[N+1];
 		
-		for (int n = 1; n <= N; n++)
-			dijkstra(n, X, goDist, true);
+		// 최소 가중치 배열 최대 값으로 정의
+		Arrays.fill(goDist, INF);
+		Arrays.fill(reverseDist, INF);
 		
-		for (int n = 1; n <= N; n++)
-			dijkstra(X, n, backDist, false);
-			
+		// X의 관점에서 다익스트라 실행
+		dijkstra(X, goDist, goEdges);
+		dijkstra(X, reverseDist, reverseEdges);
 		
-//		System.out.println(Arrays.toString(goDist));
-//		System.out.println(Arrays.toString(backDist));
-//		System.out.println(Arrays.toString(ans));
+		int ans = 0;
+		for (int i = 1; i <= N; i++) {
+			ans = Math.max(ans, goDist[i] + reverseDist[i]);
+		}
 		
-		System.out.println(Arrays.stream(ans).max().getAsInt());
-		
-		
+		System.out.println(ans);
 		
 	}
 	
-	private static void dijkstra(int start, int goal, int[] result, boolean go) {
+	private static void dijkstra(int start, int[] minValue, ArrayList<ArrayList<Edge>> adjList) {
 		
 		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.offer(new Edge(start, 0));
 		
-		int[] minValue = new int[N+1];
-		Arrays.fill(minValue, INF);
+		minValue[start] = 0;
 		
-		for (Edge edge : adjList.get(start)) {
+		while (!pq.isEmpty()) {
 			
-			boolean[] visited = new boolean[N+1];
-			visited[start] = true;
+			Edge cur = pq.poll();
 			
-			pq.offer(edge);
+			if (cur.weight > minValue[cur.vertex]) continue;
 			
-			minValue[start] = 0;
-			minValue[edge.vertex] = edge.weight;
-			
-			int first_weight = edge.weight;
-			while (!pq.isEmpty()) {
+			for (Edge next : adjList.get(cur.vertex)) {
 				
-				Edge cur = pq.poll();
-				
-				if (cur.vertex == goal) {
-					if (minValue[goal] == INF)
-						minValue[goal] = first_weight;
-					if (go) {
-						result[start] = Math.min(result[start], minValue[goal]);  // 갈 때
-					}
-					else result[goal] = Math.min(result[goal], minValue[goal]);  // 올 때
-					break;
-				}
-				
-				visited[cur.vertex] = true;
-				
-				for (Edge next : adjList.get(cur.vertex)) {
-					if (!visited[next.vertex] && minValue[next.vertex] > minValue[cur.vertex] + next.weight) {
-						minValue[next.vertex] = minValue[cur.vertex] + next.weight;
-						pq.add(new Edge(next.vertex, minValue[next.vertex]));
-					}
+				if (minValue[next.vertex] > minValue[cur.vertex] + next.weight) {
+					minValue[next.vertex] = minValue[cur.vertex] + next.weight;
+					
+					// PQ에 전달하는 가중치는 최소 가중치를 전달해야 한다.
+					pq.offer(new Edge(next.vertex, minValue[next.vertex]));
 				}
 			}
 			
 		}
-		
-		if (go) ans[start] += result[start];
-		else ans[goal] += result[goal];
 	}
 
 }
